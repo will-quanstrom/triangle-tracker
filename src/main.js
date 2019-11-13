a9b77eb5299698910e44728e672d8cf58f801c1b
wanderlust22 <wquanstr215@gmail.com>
2019-11-13 10:06:18 -0800

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
+import $ from 'jquery';
+import { CHANGENAME } from './CHANGENAME';
+
+$(document).ready(function() {
+});
