3b808d02482ba7ecc9f51cb51bf0b21e68e78d3f
Jake Ash and Will Quanstrom <jake@>
2019-11-13 11:11:27 -0800

    build a triangle tracker using jest tests to make sure its working

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
     new UglifyJsPlugin({ sourceMap: true }),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'CHANGENAME',
+      title: 'Triangle Tracker',
       template: './src/index.html',
       inject: 'body'
     })
